305d6e2eb3be1e34b2ca3175331187e705d853cf
yeahcheung <realloveyou@foxmail.com>
2016-02-29 15:21:58 +0800

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+exec "$@"
+sed -i "s/sitename/${SITE_NAME}/g" /etc/nginx/conf.d/default.conf
+sitePath = "/opt/${SITE_NAME}"
+[ ! -d "$sitePath" ] && mkdir "$sitePath"
+RUN /usr/bin/htpasswd -c -d -b /usr/share/nginx/htpasswd root $AUTH_PASSWORD
